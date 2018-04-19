a915998ca275caf04de9c5ac81d2640d85a60bf0
Robert Deliu <deliu.robert11@gmail.com>
2018-04-19 17:52:09 +0300

    Added ScrollView for user info

---
+++ app/src/main/java/com/robertdeliu/stackoverflowtop10users/UserInfoActivity.java
@@ -0,0 +1,51 @@
+package com.robertdeliu.stackoverflowtop10users;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    String name;
+    String userPhoto;
+    String reputation;
+    String location;
+    String[] badges;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_info_layout);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        userPhoto = intent.getStringExtra("user photo");
+        reputation = intent.getStringExtra("reputation");
+        location = intent.getStringExtra("location");
+        badges = intent.getStringArrayExtra("badges");
+
+        ViewHolder holder = new ViewHolder();
+
+        holder.nameTextView = findViewById(R.id.name);
+        holder.userImageView = findViewById(R.id.userPhoto);
+        holder.location = findViewById(R.id.location);
+        holder.reputation = findViewById(R.id.reputation);
+        holder.goldBadges = findViewById(R.id.goldBadges);
+        holder.silverBadges = findViewById(R.id.silverBadges);
+        holder.bronzeBadges = findViewById(R.id.bronzeBadges);
+
+        Picasso.get().load(userPhoto).into(holder.userImageView);
+
+        holder.nameTextView.setText(String.format("Name: %s", name));
+        holder.reputation.setText(String.format("%s reputation", reputation));
+        holder.location.setText(String.format("Location: %s", location));
+        holder.goldBadges.setText(String.format("Gold badges: %s", badges[2]));
+        holder.silverBadges.setText(String.format("Silver badges: %s", badges[1]));
+        holder.bronzeBadges.setText(String.format("Bronze badges: %s", badges[0]));
+
+
+    }
+}
---
+++ app/src/main/java/com/robertdeliu/stackoverflowtop10users/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.robertdeliu.stackoverflowtop10users;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder {
+    public TextView nameTextView;
+    public ImageView userImageView;
+    public ImageView positionInTop;
+    public TextView reputation;
+    public TextView location;
+    public TextView goldBadges;
+    public TextView silverBadges;
+    public TextView bronzeBadges;
+}
